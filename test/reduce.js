69fce4e6d723f80f074f25f7b7b9501917c66758
Andres Suarez <zertosh@gmail.com>
2015-10-24 22:38:32 -0700

    test cleanup on end

--- test/reduce.js
+++ test/reduce.js
@@ -7,33 +7,39 @@ var concatReduce = require('../');
 var sourceFile = fs.readFileSync(__filename, 'utf8');
 
 test('reduce sync', function(t) {
-  t.plan(2);
+  t.plan(4);
   var str = '';
+  var reduceStream = concatReduce(function(src) {
+    t.equal(typeof src, 'string');
+    return src.toUpperCase();
+  });
   fs.createReadStream(__filename)
-    .pipe(concatReduce(function(src) {
-      t.equal(typeof src, 'string');
-      return src.toUpperCase();
-    }))
+    .pipe(reduceStream)
     .on('data', function(buf) { str += buf; })
     .on('end', function() {
       t.equal(str, sourceFile.toUpperCase());
+      t.equal(reduceStream._chunks, null);
+      t.equal(reduceStream._reducer, null);
     });
 });
 
 test('reduce async', function(t) {
-  t.plan(2);
+  t.plan(4);
   var str = '';
+  var reduceStream = concatReduce(function(src) {
+    t.equal(typeof src, 'string');
+    var self = this;
+    setTimeout(function() {
+      self.push(src.toUpperCase());
+      self.push(null);
+    });
+  });
   fs.createReadStream(__filename)
-    .pipe(concatReduce(function(src) {
-      t.equal(typeof src, 'string');
-      var self = this;
-      setTimeout(function() {
-        self.push(src.toUpperCase());
-        self.push(null);
-      });
-    }))
+    .pipe(reduceStream)
     .on('data', function(buf) { str += buf; })
     .on('end', function() {
       t.equal(str, sourceFile.toUpperCase());
+      t.equal(reduceStream._chunks, null);
+      t.equal(reduceStream._reducer, null);
     });
 });
